cdd6d51d9f8ded31fc2586f323e4c5e2cd972c3d
Christopher Yu <cfyu@eng.ucsd.edu>
2017-08-08 22:29:46 +0800

    created inaccurate routes

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,45 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_params)
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+    @task.edit
+  end
+
+  def update
+    @task = @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+
+private
+
+  def task_params
+    params.require(:task).permit(:name, :description)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  # get '/tasks', to: "tasks#index"
+
+  # get '/tasks/:id', to: "tasks#show"
+
+  # get '/tasks/new', to: "tasks#new"
+
+  # post '/tasks', to: "tasks#create"
+
+  # get '/tasks/:id/edit', to: "tasks#edit"
+
+  # patch '/tasks/:id', to: "tasks#update"
+
+  # delete '/tasks/:id', to: "tasks#destroy"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks
+  # resources :tasks
 end
